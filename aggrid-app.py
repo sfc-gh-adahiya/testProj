781df0beef21e7d0483af35c511c279bb3d47c28
sfc-gh-adahiya <ankit.dahiya@snowflake.com>
2023-03-14 13:41:23 -0700

    test

--- aggrid-app.py
+++ aggrid-app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from streamlit_aggrid import AgGrid
+from st_aggrid import AgGrid
 
 df = pd.DataFrame({'col1': [1, 2, 3], 'col2': [4, 5, 6]})
 AgGrid(df)
